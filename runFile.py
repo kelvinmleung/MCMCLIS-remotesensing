7d7a9f99d9e94a2e7170fb73f2306dc1b4dd914d
Kelvin Leung <kmleung@mit.edu>
2021-02-15 15:16:26 -0500

    c2

--- runFile.py
+++ runFile.py
@@ -15,8 +15,8 @@ Nsamp = 2000000
 burn = 500000
 init = 'MAP'
 rank = 175
-constrain = False
-mcmcfolder = 'C1'
+constrain = True
+mcmcfolder = 'C2'
 ##### CONFIG #####
 
 ## SETUP ##
--- runjob.sh
+++ runjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=mcmc
+#SBATCH --job-name=mcma
 #SBATCH --workdir=/master/home/kmleung/JPLproject/MCMCLIS-remotesensing
-#SBATCH --output=runjob.out
-#SBATCH --error=runjob.err
+#SBATCH --output=runjob2.out
+#SBATCH --error=runjob2.err
  
 python runFile.py
