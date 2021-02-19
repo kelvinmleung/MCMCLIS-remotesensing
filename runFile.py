5779feb8fd1e8f0d8c4518312afaa53965985be9
Kelvin Leung <kmleung@mit.edu>
2021-02-19 18:09:55 -0500

    C7

--- runFile.py
+++ runFile.py
@@ -13,9 +13,9 @@ from mcmcIsofit import MCMCIsofit
 ##### CONFIG #####
 Nsamp = 6000000
 burn = 1000000
-init = 'truth'
-rank = 100
-mcmcfolder = 'B8'
+init = 'MAP'
+rank = 175
+mcmcfolder = 'C7'
 ##### CONFIG #####
 
 ## SETUP ##
