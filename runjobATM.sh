e212bea04f482b9e3514dfb1385f3a8bcf330a49
Kelvin Leung <kelvinleung08@hotmail.com>
2021-04-07 11:24:16 -0400

    mv file comamnd

--- runjobATM.sh
+++ runjobATM.sh
@@ -6,21 +6,21 @@
 #SBATCH --exclusive
 
 mv atmRuns/runFileA1.py .
-python atmRuns/runFileA1.py
+python runFileA1.py
 mv runFileA1.py atmRuns/
 
 mv atmRuns/runFileA3.py .
-python atmRuns/runFileA3.py
+python runFileA3.py
 mv runFileA3.py atmRuns/
 
 mv atmRuns/runFileA5.py .
-python atmRuns/runFileA5.py
+python runFileA5.py
 mv runFileA5.py atmRuns/
 
 mv atmRuns/runFileA7.py .
-python atmRuns/runFileA7.py
+python runFileA7.py
 mv runFileA7.py atmRuns/
 
 mv atmRuns/runFileA9.py .
-python atmRuns/runFileA9.py
+python runFileA9.py
 mv runFileA9.py atmRuns/
