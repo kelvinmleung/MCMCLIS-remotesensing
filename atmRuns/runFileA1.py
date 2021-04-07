ac0154593f7b26f1901fec5b3bf90adca087400e
Kelvin Leung <kelvinleung08@hotmail.com>
2021-04-07 11:09:44 -0400

    atm run files

---
+++ atmRuns/runFileA1.py
@@ -0,0 +1,60 @@
+import subprocess
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from isofitSetup import Setup
+from genSamples import GenerateSamples
+from regression import Regression
+from analysis import Analysis
+from mcmcIsofit import MCMCIsofit
+from plots import Plots
+
+
+##### CONFIG 1 #####
+H2O = 1.5
+mcmcfolder = 'H15'
+
+AOD = 0.1
+mcmcfolder = mcmcfolder + 'A1'
+
+
+##### CONFIG 2 #####
+Nsamp = 6000
+burn = 3000
+init = 'truth'
+rank = 100
+LIS = True
+
+
+## SETUP ##
+wv, ref = np.loadtxt('setup/data/petunia/petunia_reflectance.txt').T
+atm = [AOD, H2O]
+setup = Setup(wv, ref, atm, mcmcdir=mcmcfolder)
+g = GenerateSamples(setup)
+r = Regression(setup)
+a = Analysis(setup, r)
+
+## MCMC #
+if init == 'MAP':
+    x0 = setup.isofitMuPos
+elif init == 'truth':
+    x0 = setup.truth
+mcmcfolder = mcmcfolder + '_init' + init + '_rank' + str(rank)
+
+m = MCMCIsofit(setup, a, Nsamp, burn, x0, 'AM')
+m.initMCMC(LIS=LIS, rank=rank) # specify LIS parameters
+
+
+start_time = time.time()
+m.runAM()
+MCMCmean, MCMCcov = m.calcMeanCov()
+setup.plotPosterior(m.linMuPos, m.linGammaPos, MCMCmean, MCMCcov)
+
+## MCMC Diagnostics ##
+indSet = [30,40,90,100,150,160,250,260,425,426]
+m.diagnostics(MCMCmean, MCMCcov, indSet)
+np.savetxt(setup.mcmcDir + 'runtime.txt', np.array([time.time() - start_time]))
+
+
+
