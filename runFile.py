b620d8d51e7b92a0e802a1dc533456c1b5722a1a
Kelvin Leung <kelvinleung08@hotmail.com>
2021-06-22 17:58:58 -0400

    G12

--- runFile.py
+++ runFile.py
@@ -14,10 +14,10 @@ from mcmcIsofit import MCMCIsofit
 ##### CONFIG #####
 Nsamp = 6000000
 burn = 1000000
-init = 'MAP'
+init = 'truth'
 rank = 100
 LIS = True
-mcmcfolder = 'G11'
+mcmcfolder = 'G12'
 ##### CONFIG #####
 
 ## SETUP ##
