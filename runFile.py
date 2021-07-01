555125a54a4c8fb72c985e99e93574893cf27feb
Kelvin Leung <kmleung@mit.edu>
2021-06-30 21:42:50 -0400

    G11

--- runFile.py
+++ runFile.py
@@ -12,13 +12,13 @@ from mcmcIsofit import MCMCIsofit
 
 
 ##### CONFIG #####
-Nsamp = 6000
-burn = 2000
+Nsamp = 6000000
+burn = 2000000
 init = 'MAP'
 rank = 100
 LIS = True
-mcmcfolder = 'G1'
-thinning = 10
+mcmcfolder = 'G11'
+thinning = 20
 ##### CONFIG #####
 
 f = FileProcessing()
